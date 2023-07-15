21e3c625283667e2d66bc364831aca3abbc72050
ElyhanSirait <elyhansirait@gmail.com>
2023-07-15 17:47:02 +0800

    Proyek Akhir ATM

---
+++ build/classes/uaspbo1/rekening.rs
@@ -0,0 +1,1 @@
+uaspbo1.Rekening
---
+++ src/uaspbo1/kurang.java
@@ -0,0 +1,8 @@
+package uaspbo1;
+ // Class exception untuk saldo tidak mencukupi
+class kurang extends Exception {
+    public kurang(String message) {
+        super(message);
+    }
+}  
+
